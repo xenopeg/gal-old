64a43cca173745d5f40ab5a1573ba9253f0d5e53
Reef <4822785+xenopeg@users.noreply.github.com>
2024-04-10 14:39:33 +0100

    Changed NX for yarn workspaces

--- apps/gal-backend/src/main.ts
+++ apps/gal-backend/src/main.ts
@@ -1,21 +1,30 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: path.join(__dirname,'..','..','gal-lib','.env'),
+})
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/ping', (req, res) => {
+app.post('/auth', (req, res) => {
   res.send({ message: 'Welcome to gal-backend!' });
 });
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+app.get('/ping', (req, res) => {
+  res.send({ message: 'pong' });
 });
+
+app.use((req, res, next) => {
+  res.status(404).send("Not found")
+})
+
+const port = process.env.API_PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/ping`);
+});
+
 server.on('error', console.error);
